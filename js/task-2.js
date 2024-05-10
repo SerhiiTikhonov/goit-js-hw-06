dd7bb499e20c50823483f55e7c20804097efb268
Serhii Tikhonov <157369790+SerhiiTikhonov@users.noreply.github.com>
2024-05-10 12:56:43 +0300

    task6

---
+++ js/task-2.js
@@ -0,0 +1,31 @@
+class Storage {
+    #item;
+    constructor(params) {
+        this.#item = params;
+    }
+    getItems() {
+        return this.#item;
+    }
+    addItem(newItem) {
+        this.#item.push(newItem);
+    }
+    removeItem(itemToRemove) {
+        const index = this.#item.indexOf(itemToRemove);
+        if (index !== -1) {
+            this.#item.splice(index, 1);
+        }
+    }
+}
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
